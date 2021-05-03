83a99f765e807714f6315556f69696a669ba988e
raphaelnicolini <raphaelnicolini1991@gmail.com>
2021-05-03 15:56:31 -0300

    exercicio-variaveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,15 @@
+const nome
+let idade
+
+console.log("Seu nome é", typeof nome)
+console.log("sua idade é", typeof idade)
+
+/* erro de variável */
+
+const nomeDoUsuario = prompt("Qual é seu nome?")
+let idadeDoUsuario = prompt ("Qual é sua idade")
+
+console.log(nomeDoUsuario)
+console.log(idadeDoUsuario)
+
+alert("Olá!", nomeDoUsuario, "você tem", idadeDoUsuario, "anos")
