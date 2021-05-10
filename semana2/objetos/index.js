9d63b352e23ef397c8858c44fa4fbdf57bdb7966
raphaelnicolini <raphaelnicolini1991@gmail.com>
2021-05-10 15:03:19 -0300

    exercicio-objetos

---
+++ semana2/objetos/index.js
@@ -0,0 +1,58 @@
+//------------------Exercícios de interpretação de código
+/*
+
+1)
+a) 
+
+"Matheus Nachtergale"
+3
+"Globo"
+
+
+2)
+a)
+
+"Juca"
+
+"Juba"
+3
+"SRD"
+
+"Jubo"
+
+
+3)
+a)
+
+false
+null
+
+
+b)Faltou o valor da altura
+
+*/
+
+//-------------------------------------Exercícios de escrita de código
+
+//1)
+//a)
+const pessoa = {
+    nome: "Raphael",
+    apelidos: ["rapha, fael, nico"]
+}
+ console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos}`)
+
+ //b)
+ const novoObjeto = {
+     ...pessoa,
+     apelidos: ["faraco", "rafinha", "rafão"]  
+ }
+ console.log(novoObjeto)
+
+
+ 
+//2
+//a)
+
+
+
