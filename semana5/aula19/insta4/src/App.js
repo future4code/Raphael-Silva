3cdf8cd27a35c58e1002b483f786df99188b7817
raphaelnicolini <raphaelnicolini1991@gmail.com>
2021-06-02 16:15:03 -0300

    quase nada

---
+++ semana5/aula19/insta4/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Raphael'}
+          fotoUsuario={'https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ'}
+          fotoPost={'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68'}
+        />
+
+        <Post
+          nomeUsuario={'Noob'}
+          fotoUsuario={'https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY'}
+          fotoPost={'https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w'}
+        />
+
+
+
+
+
+
+
+
+
+      </MainContainer>
+
+      
+    );
+  }
+}
+
+export default App;
