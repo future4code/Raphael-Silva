c78b06ca32f3139cf39fc438d8a27e329d14625c
raphaelnicolini <raphaelnicolini1991@gmail.com>
2021-06-04 15:34:10 -0300

    insta

--- semana5/aula19/insta4/src/App.js
+++ semana5/aula19/insta4/src/App.js
@@ -1,47 +1,27 @@
 import React from 'react';
-import styled from 'styled-components'
+import './App.css';
 import Post from './components/Post/Post';
-
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
 
 class App extends React.Component {
   render() {
     return (
-      <MainContainer>
+      <div className={'app-container'}>
         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
-
         <Post
-          nomeUsuario={'Raphael'}
-          fotoUsuario={'https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ'}
-          fotoPost={'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68'}
+          nomeUsuario={'joao'}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
         />
-
         <Post
-          nomeUsuario={'Noob'}
-          fotoUsuario={'https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY'}
-          fotoPost={'https://i.picsum.photos/id/100/2500/1656.jpg?hmac=gWyN-7ZB32rkAjMhKXQgdHOIBRHyTSgzuOK6U0vXb1w'}
+          nomeUsuario={'amanda'}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
         />
-
-
-
-
-
-
-
-
-
-      </MainContainer>
-
-      
+      </div>
     );
   }
 }
--- semana5/aula19/insta4/src/components/Post/Post.js
+++ semana5/aula19/insta4/src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import './Post.css'
 
 import {IconeComContador} from '../IconeComContador/IconeComContador'
 
@@ -7,38 +7,6 @@ import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
-
-const PostContainer = styled.div`
-  border: 1px solid gray;
-  width: 300px;
-  margin-bottom: 10px;
-`
-
-const PostHeader = styled.div`
-  height: 40px;
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-`
-
-const PostFooter = styled.div`
-  height: 40px;
-  display: flex;
-  align-items: center;
-  padding: 0 10px;
-  justify-content: space-between;
-`
-
-const UserPhoto = styled.img`
-  height: 30px;
-  width: 30px;
-  margin-right: 10px;
-  border-radius: 50%;
-`
-
-const PostPhoto = styled.img`
-  width: 100%;
-`
 
 class Post extends React.Component {
   state = {
@@ -49,7 +17,18 @@ class Post extends React.Component {
   }
 
   onClickCurtida = () => {
-    console.log('Curtiu!')
+    let novoNumeroCurtidas
+
+    if(this.state.curtido) {
+      novoNumeroCurtidas = this.state.numeroCurtidas - 1
+    } else {
+      novoNumeroCurtidas = this.state.numeroCurtidas + 1
+    }
+
+    this.setState({
+      curtido: !this.state.curtido,
+      numeroCurtidas: novoNumeroCurtidas
+    })
   }
 
   onClickComentario = () => {
@@ -80,15 +59,15 @@ class Post extends React.Component {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
 
-    return <PostContainer>
-      <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+    return <div className={'post-container'}>
+      <div className={'post-header'}>
+        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
         <p>{this.props.nomeUsuario}</p>
-      </PostHeader>
+      </div>
 
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-      <PostFooter>
+      <div className={'post-footer'}>
         <IconeComContador
           icone={iconeCurtida}
           onClickIcone={this.onClickCurtida}
@@ -100,9 +79,9 @@ class Post extends React.Component {
           onClickIcone={this.onClickComentario}
           valorContador={this.state.numeroComentarios}
         />
-      </PostFooter>
+      </div>
       {componenteComentario}
-    </PostContainer>
+    </div>
   }
 }
 
